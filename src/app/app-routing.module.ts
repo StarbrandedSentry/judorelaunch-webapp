05bea0dac93e95cd948194351b8ffc27b88575e7
StarbrandedSentry <hierarchhero@gmail.com>
2019-10-02 19:20:01 +0800

    working on more fonts 😌😌😌

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+  {path: 'welcome', component: FrontPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
